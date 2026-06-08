92dd0e27df5a9d9925f72291aa835d77734437e7
Miguel Romão <miguelbr051102@gmail.com>
2026-06-08 16:46:09 -0300

    Permite configurar origem da API no mobile

--- mobile/src/api/api.js
+++ mobile/src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const API_ORIGIN =
+    process.env.EXPO_PUBLIC_API_ORIGIN ||
     "https://guardioes-urbanos-production.up.railway.app";
 
 const api = axios.create({
